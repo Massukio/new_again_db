b73b6c2291bc8abffac7f57bd1f04c6cd2e63411
Timmy Lin <timmy7124@gmail.com>
2025-04-02 21:11:05 +0800

    Refactor main window setup to include button and input configurations

--- refactor/app/main.py
+++ refactor/app/main.py
@@ -17,14 +17,18 @@ class MainWindow:
         self.window.setObjectName('MainWindow')
         self.central_widget = QtWidgets.QWidget(self.window)
         self.central_widget.setObjectName('central_widget')
+        self.window.setCentralWidget(self.central_widget)
+
+        # Setup grid and buttons
         self.setup_grids()
+        self.setup_buttons()
+        self.setup_inputs()
 
     def setup_grids(self):
         self.table_widget = QtWidgets.QWidget(self.central_widget)
         self.table_widget.setObjectName("table_grid_widget")
         self.table_widget.setGeometry(QtCore.QRect(
             self.margin, self.margin,750 - self.margin, 400 - self.margin))
-        self.table_widget.setStyleSheet("background-color: lightgray;")
 
         self.table_grid = QtWidgets.QGridLayout(self.table_widget)
         self.table_grid.setObjectName("table_layout")
@@ -32,26 +36,65 @@ class MainWindow:
             self.margin, self.margin, self.margin, self.bottom_margin)
 
         self.btn_widget = QtWidgets.QWidget(self.central_widget)
+        self.btn_widget.setObjectName("button_grid_widget")
         self.btn_widget.setGeometry(QtCore.QRect(
             750+self.margin, self.margin, 200-self.margin, 400-self.margin))
-        self.btn_widget.setObjectName("button_grid_widget")
-        self.btn_widget.setStyleSheet("background-color: red;")
 
         self.btn_grid = QtWidgets.QGridLayout(self.btn_widget)
+        self.btn_grid.setObjectName("button_layout")
         self.btn_grid.setContentsMargins(
             self.margin, self.margin, self.margin, self.bottom_margin)
-        self.btn_grid.setObjectName("button_layout")
 
         self.input_widget = QtWidgets.QWidget(self.central_widget)
+        self.input_widget.setObjectName("input_grid_widget")
         self.input_widget.setGeometry(QtCore.QRect(
             self.margin, 400+self.margin,
             750-self.margin, 100-self.bottom_margin))
-        self.input_widget.setObjectName("input_grid_widget")
 
         self.input_grid = QtWidgets.QGridLayout(self.input_widget)
+        self.input_grid.setObjectName("input_layout")
         self.input_grid.setContentsMargins(
             self.margin, self.margin, self.margin, self.bottom_margin)
-        self.input_grid.setObjectName("input_layout")
+
+    def setup_buttons(self):
+        self.connect_button = QtWidgets.QPushButton("連接資料庫", self.btn_widget)
+        self.connect_button.setObjectName("connect_button")
+        self.btn_grid.addWidget(self.connect_button, 0, 0, 1, 1)
+
+        self.view_all_button = QtWidgets.QPushButton("瀏覽全部資料", self.btn_widget)
+        self.view_all_button.setObjectName("view_all_button")
+        self.btn_grid.addWidget(self.view_all_button, 1, 0, 1, 1)
+
+        self.add_button = QtWidgets.QPushButton("新增資料", self.btn_widget)
+        self.add_button.setObjectName("add_button")
+        self.btn_grid.addWidget(self.add_button, 2, 0, 1, 1)
+
+        self.delete_button = QtWidgets.QPushButton("刪除所選資料", self.btn_widget)
+        self.delete_button.setObjectName("delete_button")
+        self.btn_grid.addWidget(self.delete_button, 3, 0, 1, 1)
+
+        self.modify_button = QtWidgets.QPushButton("修改所選資料", self.btn_widget)
+        self.modify_button.setObjectName("modify_button")
+        self.btn_grid.addWidget(self.modify_button, 4, 0, 1, 1)
+
+        self.backup_button = QtWidgets.QPushButton("備份資料庫", self.btn_widget)
+        self.backup_button.setObjectName("backup_button")
+        self.btn_grid.addWidget(self.backup_button, 5, 0, 1, 1)
+
+    def setup_inputs(self):
+        self.search_combo_box = QtWidgets.QComboBox(self.input_widget)
+        self.search_combo_box.setObjectName("search_combo_box")
+        self.search_combo_box.addItem("車牌查詢")
+        self.search_combo_box.addItem("電話查詢")
+        self.input_grid.addWidget(self.search_combo_box, 0, 0, 1, 1)
+
+        self.plate_input1 = QtWidgets.QLineEdit(self.input_widget)
+        self.plate_input1.setObjectName("plate_input1")
+        self.input_grid.addWidget(self.plate_input1, 0, 1, 1, 1)
+
+        self.plate_input2 = QtWidgets.QLineEdit(self.input_widget)
+        self.plate_input2.setObjectName("plate_input2")
+        self.input_grid.addWidget(self.plate_input2, 0, 2, 1, 1)
 
 
     def setup(self):
